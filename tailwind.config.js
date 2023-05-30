0768dbb1db0869845d6073d2193ae405e1972d81
Aimun-Aziz <aimunaziz04@gmail.com>
2023-05-30 22:10:00 +0500

    first commit

---
+++ tailwind.config.js
@@ -0,0 +1,19 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["*"],
+  theme: {
+    extend: {
+      width:{
+        82:'99.443333%',
+        85:'99.822222%',
+        97:'99.888888%'
+      },
+      height:{
+        82:'99.443333%',
+        85:'99.822222%'
+      }
+    },
+  },
+  plugins: [],
+}
+
